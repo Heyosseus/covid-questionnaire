d0912ab30c332f05c072f908101326b3f71b1f03
heyosseus <ratiruxadzee@gmail.com>
2023-05-07 15:18:01 +0400

    feat: add vuex and router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,33 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import store from './store';
+import './style.css';
 
-import './assets/main.css'
+const routes = [
+  { path: '/', component: () => import('./pages/Home.vue') },
+  {
+    path: '/identification',
+    component: () => import('./pages/PersonalInfo.vue'),
+  },
+  {
+    path: '/survey',
+    component: () => import('./pages/Survey.vue'),
+  },
+  {
+    path: '/vaccine',
+    component: () => import('./pages/Vaccine.vue'),
+  },
+  { path: '/tips', component: () => import('./pages/Tips.vue') },
+  {
+    path: '/thanks',
+    component: () => import('./pages/Thanks.vue'),
+  },
+];
 
-createApp(App).mount('#app')
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+createApp(App).use(store).use(router).mount('#app');
