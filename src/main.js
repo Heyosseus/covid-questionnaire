403102b40722ba60ddd4db1f0c64eb6d09c7b659
heyosseus <ratiruxadzee@gmail.com>
2023-05-08 17:48:18 +0400

    feat: add name property to the routes

--- src/main.js
+++ src/main.js
@@ -5,22 +5,34 @@ import store from './store';
 import './style.css';
 
 const routes = [
-  { path: '/', component: () => import('./pages/Home.vue') },
   {
-    path: '/identification',
+    path: '/',
+    name: 'home',
+    component: () => import('./pages/Home.vue'),
+  },
+  {
+    path: '/personal',
+    name: 'personal',
     component: () => import('./pages/PersonalInfo.vue'),
   },
   {
     path: '/survey',
+    name: 'survey',
     component: () => import('./pages/Survey.vue'),
   },
   {
     path: '/vaccine',
+    name: 'vaccine',
     component: () => import('./pages/Vaccine.vue'),
   },
-  { path: '/tips', component: () => import('./pages/Tips.vue') },
+  {
+    path: '/tips',
+    name: 'tips',
+    component: () => import('./pages/Tips.vue'),
+  },
   {
     path: '/thanks',
+    name: 'thanks',
     component: () => import('./pages/Thanks.vue'),
   },
 ];
