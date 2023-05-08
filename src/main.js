4c68665b9da27b210cfa6054d60db1ce88dce2f3
heyosseus <ratiruxadzee@gmail.com>
2023-05-08 17:58:26 +0400

    refactor: refactor router

--- src/main.js
+++ src/main.js
@@ -1,45 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import { createRouter, createWebHistory } from 'vue-router';
+import router from './routes/index';
 import store from './store';
 import './style.css';
-
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: () => import('./pages/Home.vue'),
-  },
-  {
-    path: '/personal',
-    name: 'personal',
-    component: () => import('./pages/PersonalInfo.vue'),
-  },
-  {
-    path: '/survey',
-    name: 'survey',
-    component: () => import('./pages/Survey.vue'),
-  },
-  {
-    path: '/vaccine',
-    name: 'vaccine',
-    component: () => import('./pages/Vaccine.vue'),
-  },
-  {
-    path: '/tips',
-    name: 'tips',
-    component: () => import('./pages/Tips.vue'),
-  },
-  {
-    path: '/thanks',
-    name: 'thanks',
-    component: () => import('./pages/Thanks.vue'),
-  },
-];
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes,
-});
 
 createApp(App).use(store).use(router).mount('#app');
---
+++ src/routes/index.js
@@ -0,0 +1,41 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('../pages/Home.vue'),
+  },
+  {
+    path: '/personal',
+    name: 'personal',
+    component: () => import('../pages/PersonalInfo.vue'),
+  },
+  {
+    path: '/survey',
+    name: 'survey',
+    component: () => import('../pages/Survey.vue'),
+  },
+  {
+    path: '/vaccine',
+    name: 'vaccine',
+    component: () => import('../pages/Vaccine.vue'),
+  },
+  {
+    path: '/tips',
+    name: 'tips',
+    component: () => import('../pages/Tips.vue'),
+  },
+  {
+    path: '/thanks',
+    name: 'thanks',
+    component: () => import('../pages/Thanks.vue'),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
